dbd8456f8c7a51065e4da0bba8ecb28c995c1cff
jeromecamilleri <jerome.camilleri@gmail.com>
2023-12-22 14:02:26 +0100

    Update Numéros de téléphone - Code Golf.py
    une soltion plus courte ajoutée

--- Numéros de téléphone - Code Golf.py
+++ Numéros de téléphone - Code Golf.py
@@ -39,3 +39,21 @@ print(str(nbAjout))
 #Debug telephone... 15
 #28
 #Success
+
+# une solution trouvee sur internet beaucoup plus courte !
+# https://www.codingame.com/ide/puzzle/telephone-numbers
+#import sys
+#import math
+#data = {}
+#n = int(input())
+#cnt = 0
+#for i in range(n):
+#    telephone = input()
+#    temp = data
+#    for t in telephone:
+#        val = temp.get(t, None)
+#        if val == None:
+#            cnt+=1
+#        temp = temp.setdefault(t, {})
+# The number of elements (referencing a number) stored in the structure.
+#print(cnt)
